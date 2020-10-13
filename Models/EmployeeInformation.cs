ae9901338ee6620904e9761e9dfcca21cd5d086e
Robert Williams <rw847916@ohio.edu>
2020-10-13 12:33:08 -0400

    Added a model class for employee information

---
+++ Models/EmployeeInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team9.Models
+{
+    public class EmployeeInformation
+    {
+
+
+
+    }
+}
