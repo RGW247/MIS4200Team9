91d24b8f34ca0ecca629313a4a9f0cc89e2da090
Alex Gray <ag410316@ohio.edu>
2020-11-04 11:45:23 -0500

    Added new models position and emoloyee, centered profile page

---
+++ Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team9.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int employeeID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string fullName
+        { get
+            { return lastName + ", " + firstName; }
+                
+                }
+        public DateTime hireDate { get; set; }
+        [ForeignKey("workerID")]
+
+        public ICollection<Position> worker { get; set; }
+        [ForeignKey("bossID")]
+
+        public ICollection<Position> boss { get; set; }
+
+
+    }
+}
